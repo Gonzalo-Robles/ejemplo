6d258d65b19350339a31d23247098a840b80e72e
Gonzalo Robles <gonzalorobles1984@gmail.com>
2022-11-20 21:15:21 -0300

    Clase de js

---
+++ proyecto/primerproyecto/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+const Navbar = () =>{
+    return (
+        <nav className='navbar'>
+            <h1> MyAPP </h1>
+            <button className="boton1"> Nosotros</button>
+            <button className="boton1"> Productos</button>
+            <button className="boton1"> Contacto</button>
+        </nav>
+    )
+}
+
+export default Navbar
